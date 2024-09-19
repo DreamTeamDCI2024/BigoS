a20535a6defbce3a634c517ddd353b6fe9e2caef
Marcomano28 <masnopuedo2021@gmail.com>
2024-09-19 11:59:50 +0200

    fixing the videos of rooms

--- client/src/utils/roomsData.js
+++ client/src/utils/roomsData.js
@@ -3,7 +3,7 @@ const roomsData = {
     Livingroom: {
       roomName: 'Living Room',
       imageSrc: '/images/LivingRoom/LivingRoom-large.webp',
-      videoSrc: '/videos/LivingRoomVideo.mp4',
+      videoSrc: '/videos/LivingRoomVideo.webm',
       preloaderTitle1: 'Bigos',
       preloaderTitle2: 'Designe Studio',
       preloaderSubtitle: 'Explore our range of stylish and comfortable living room furniture.',
@@ -11,7 +11,7 @@ const roomsData = {
     Bedroom: {
       roomName: 'Bedroom',
       imageSrc: '/images/BedRoom/BedRoom3-large.webp',
-      videoSrc: '/videos/BedroomVideo.mp4',
+      videoSrc: '/videos/BedroomVideo.webm',
       preloaderTitle1: 'Bigos',
       preloaderTitle2: 'Creative Agency',
       preloaderSubtitle: "Create your personal sanctuary with our serene bedroom furniture, perfect for peaceful nights and calm mornings.",
@@ -19,7 +19,7 @@ const roomsData = {
     Bathroom: {
       roomName: 'Bathroom',
       imageSrc: '/images/BathRoom/BathRoom-large.webp',
-      videoSrc: '/videos/BathroomVideo.mp4',
+      videoSrc: '/videos/BathroomVideo.webm',
       preloaderTitle1: 'Bigos',
       preloaderTitle2: 'Interior Solutions',
       preloaderSubtitle: 'Transform your bathroom into a spa-like retreat with our luxurious furniture collection',
@@ -27,7 +27,7 @@ const roomsData = {
     Balcony: {
       roomName: 'Balcony',
       imageSrc: '/images/Balcony/Balcony-large.webp',
-      videoSrc: '/videos/BalconyVideo.mp4',
+      videoSrc: '/videos/BalconyVideo.webm',
       preloaderTitle1: 'Bigos',
       preloaderTitle2: 'Team project',
       preloaderSubtitle: 'Create a serene escape on your balcony with our comfortable and minimalist furniture',
